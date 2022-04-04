30b190f9549c11284faca284dab5233e58e29f77
roem09 <muhammadrum04@gmail.com>
2022-04-04 10:19:53 +0700

    first commit

--- application/controllers/Tes.php
+++ application/controllers/Tes.php
@@ -293,7 +293,7 @@ class Tes extends MY_Controller {
 
         $skor = round($skor);
         
-        $peserta['no_doc'] = "{$peserta['no_doc']}/TFL/BKL/{$tahun}";
+        $peserta['no_doc'] = "{$peserta['no_doc']}/TFL/BKL/{$peserta['tahun']}";
 
         $peserta['config'] = $this->tes->config();
         $peserta['id_tes'] = $peserta['id_tes'];
